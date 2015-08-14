2ed85bd04d63a76e96479e64850861a70fd166a8
gguthe <gguthe@rjmetrics.com>
2015-08-14 13:59:02 -0400

    Use project.clj for maven deploy
    Refs: https://github.com/RJMetrics/core-services/issues/8

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.clojars.gguthe/phpass "1.0.0"
+  :description "FIXME: write description"
+  :url "https://github.com/RJMetrics/PHPass.git"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [])
