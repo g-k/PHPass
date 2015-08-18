28dfc7a6d1d0c7467be28dd2b3a6855854fab102
gguthe <gguthe@rjmetrics.com>
2015-08-18 16:01:26 -0400

    Update java src paths

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.gguthe/phpass "1.0.1"
+(defproject org.clojars.gguthe/phpass "1.0.2-SNAPSHOT"
   :description "Fork of PHPass"
   :url "https://github.com/RJMetrics/PHPass.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :java-source-paths ["src/main"]
+  :java-source-paths ["src/main/java/org/clojars/gguthe/phpass"]
   :dependencies [])
