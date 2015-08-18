78fa2e879a54839f5f693db24497eac761ee3da8
gguthe <gguthe@rjmetrics.com>
2015-08-18 15:51:16 -0400

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojars.gguthe/phpass "1.0.1"
+  :description "Fork of PHPass"
+  :url "https://github.com/RJMetrics/PHPass.git"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :java-source-paths ["src/main"]
+  :dependencies [])
