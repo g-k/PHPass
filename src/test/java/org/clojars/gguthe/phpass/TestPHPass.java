0a7440e214f37c747213b8ad3955c96fd510430b
Alexandra Smith <asmith@rjmetrics.com>
2016-01-15 10:14:49 -0500

    swtich to BCrypt

--- src/test/java/org/clojars/gguthe/phpass/TestPHPass.java
+++ src/test/java/org/clojars/gguthe/phpass/TestPHPass.java
@@ -8,50 +8,57 @@ import org.junit.Test;
 public class TestPHPass extends TestCase {
     static final String correct = "test12345";
     static final String wrong = "test12346";
-    static final String hash = "$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0";
+    static final String hashMD5 = "$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0";
+    static final String hashBCrypt = "$2a$10$Ap88hInx7k41c.WE/CfI7u/XEjAAdOmwoFNfveT6pb6F7/uJ.4r9e";
+
     static final String correctpl = "ąćęłóńśźż";
     static final String wrongpl = "acelonszz";
-    static final String hashpl = "$P$ByXWbzDvVAJ0jxvNp5sv4xYPIRXJJ1.";
-    static final PHPass phpass = new PHPass(8);
+    static final String hashplMD5 = "$P$ByXWbzDvVAJ0jxvNp5sv4xYPIRXJJ1.";
+    static final String hashBCrypt = "$2a$10$uVWyXfgJlCTXy837ZrXR1.UA2f1YbJsN19ykdDoqnfyzq26IYW9eS";
+    static final PHPass phpass = new PHPass(12);
     String hashed = phpass.HashPassword(correct);
 
     public static void main(String args[]) {
         junit.textui.TestRunner.run(TestPHPass.class);
     }
 
-
     public final void testCheckPassword_correct() {
         boolean check = phpass.CheckPassword(correct, this.hashed);
         Assert.assertTrue(check);
     }
-
-
+    
     public final void testCheckPassword_wrong() {
         boolean check = phpass.CheckPassword(wrong, this.hashed);
         Assert.assertFalse(check);
     }
 
-
     public final void testCheckPassword_givenhash_correct() {
-        boolean check = phpass.CheckPassword(correct, hash);
+        boolean check = phpass.CheckPassword(correct, hashBCrypt);
         Assert.assertTrue(check);
     }
 
+    public final void testCheckPasswordWithMD5Hash_correct() {
+        boolean check = phpass.CheckPassword(correct, hashMD5);
+        Assert.assertTrue(check);
+    }
 
     public final void testCheckPassword_givenhash_wrong() {
-        boolean check = phpass.CheckPassword(wrong, hash);
+        boolean check = phpass.CheckPassword(wrong, hashBCrypt);
         Assert.assertFalse(check);
     }
 
-
     public final void testCheckPassword_nationalCharacters_correct() {
-        boolean check = phpass.CheckPassword(correctpl, hashpl);
+        boolean check = phpass.CheckPassword(correctpl, hashplBCrypt);
         assertTrue(check);
     }
 
+    public final void testCheckPassword_nationalCharacters_correctMD5() {
+        boolean check = phpass.CheckPassword(correctpl, hashplMD5);
+        assertTrue(check);
+    }
 
     public final void testCheckPassword_nationalCharacters_wrong() {
-        boolean check = phpass.CheckPassword(wrongpl, hashpl);
+        boolean check = phpass.CheckPassword(wrongpl, hashplBCrypt);
         assertFalse(check);
     }
 
